b7c05663a3c61bf513721d3356028ad561d6e97c
AbelAnderson <Asatheabel@gmail.com>
2019-11-09 15:43:38 -0500

    #5 Added Credits Fragment

---
+++ app/src/main/java/com/example/designmaterials/CreditsFragment.java
@@ -0,0 +1,41 @@
+package com.example.designmaterials;
+
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import org.w3c.dom.Attr;
+
+import java.util.ArrayList;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CreditsFragment extends Fragment {
+
+
+    public CreditsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_credits, container, false);
+        return view;
+    }
+
+}
