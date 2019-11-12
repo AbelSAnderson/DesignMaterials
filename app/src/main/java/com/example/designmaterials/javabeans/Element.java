96868e018e0ec94d41c951820bf516ff68f2bdb8
AbelAnderson <Asatheabel@gmail.com>
2019-11-11 22:20:35 -0500

    #4 Created Element Page for displaying individual Elements

---
+++ app/src/main/java/com/example/designmaterials/javabeans/Element.java
@@ -0,0 +1,13 @@
+package com.example.designmaterials.javabeans;
+
+public class Element {
+    private String name;
+
+    public Element(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
