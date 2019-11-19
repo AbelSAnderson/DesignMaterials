58a75dfb112cb31540de2c17d95c809482b2b442
AbelAnderson <Asatheabel@gmail.com>
2019-11-19 11:35:01 -0500

    #16 Created ViewPager

--- app/src/main/java/com/example/designmaterials/fragments/ViewsFragment.java
+++ app/src/main/java/com/example/designmaterials/fragments/ViewsFragment.java
@@ -3,7 +3,11 @@ package com.example.designmaterials.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -24,10 +28,36 @@ public class ViewsFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_views, container, false);
+        View view = inflater.inflate(R.layout.fragment_views, container, false);
+
+        ((ViewPager) view.findViewById(R.id.layoutViewPager)).setAdapter(new LayoutViewPagerAdapter(getChildFragmentManager()));
+
+        return view;
+    }
+
+    private class LayoutViewPagerAdapter extends FragmentPagerAdapter {
+
+        private int[] layouts = {R.layout.layout_view_one};
+
+        public LayoutViewPagerAdapter(@NonNull FragmentManager fm) {
+            super(fm);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            if(position >= layouts.length || position < 0) {
+                return ViewFragment.newInstance(R.layout.fragment_view);
+            }
+            return ViewFragment.newInstance(layouts[position]);
+        }
+
+        @Override
+        public int getCount() {
+            return layouts.length;
+        }
     }
 
 }
