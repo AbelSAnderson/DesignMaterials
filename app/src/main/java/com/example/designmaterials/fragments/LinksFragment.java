c0f010050fe445bd03eb1b5adc4de0bf8ab90c86
AbelAnderson <Asatheabel@gmail.com>
2019-11-25 12:28:15 -0500

    #23 Created Links Screen, with two Intents
    Created Links Screen Layout
    Created Call and SMS button Functionality

---
+++ app/src/main/java/com/example/designmaterials/fragments/LinksFragment.java
@@ -0,0 +1,108 @@
+package com.example.designmaterials.fragments;
+
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.example.designmaterials.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class LinksFragment extends Fragment {
+
+    public static final int PERMISSION_TEXT = 1;
+    public static final int PERMISSION_CALL_PHONE = 2;
+
+    public LinksFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_links, container, false);
+
+        view.findViewById(R.id.textButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), Manifest.permission.SEND_SMS)) {
+                        final AlertDialog alertDialog = new AlertDialog.Builder(getContext()).setTitle("SMS Permission").setMessage("We need access to SMS in order to send a message").create();
+                        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                alertDialog.dismiss();
+                                ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.SEND_SMS}, PERMISSION_TEXT);
+                            }
+                        });
+                        alertDialog.show();
+                    } else {
+                        ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.SEND_SMS}, PERMISSION_TEXT);
+                    }
+                } else {
+                    Intent intent = new Intent(Intent.ACTION_SENDTO, Uri.parse("smsto: 0000000000"));
+                    intent.putExtra("sms_body", "I am having trouble with ");
+                    if (intent.resolveActivity(getActivity().getPackageManager()) != null) {
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(getActivity(), "No software installed to complete task", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+        view.findViewById(R.id.phoneButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), Manifest.permission.CALL_PHONE)) {
+                        final AlertDialog alertDialog = new AlertDialog.Builder(getContext()).setTitle("Phone Permission").setMessage("We need access to your phone in order to make a call.").create();
+                        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                alertDialog.dismiss();
+                                ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.CALL_PHONE}, PERMISSION_CALL_PHONE);
+                            }
+                        });
+                        alertDialog.show();
+                    } else {
+                        ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.CALL_PHONE}, PERMISSION_CALL_PHONE);
+                    }
+                } else {
+                    Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel: 0000000000"));
+                    if (intent.resolveActivity(getActivity().getPackageManager()) != null) {
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(getActivity(), "No software installed to complete task", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+        view.findViewById(R.id.emailButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        return view;
+    }
+}
